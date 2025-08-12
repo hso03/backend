ef1cbfc579c1f457ee509cf87616c363c94a0932
오성현 <fhqht303@naver.com>
2025-08-13 03:04:09 +0900

    feat : @Builder 어노테이션 추가

--- src/main/java/com/osh/backend/member/dto/MemberResponse.java
+++ src/main/java/com/osh/backend/member/dto/MemberResponse.java
@@ -1,10 +1,12 @@
 package com.osh.backend.member.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Getter
 @AllArgsConstructor
 public class MemberResponse {
