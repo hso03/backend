18122882d99c8bddc2b62564177fdcd2a941057e
오성현 <fhqht303@naver.com>
2025-08-13 03:03:49 +0900

    feat : findByMemberEmail 메서드 추가

--- src/main/java/com/osh/backend/member/repository/MemberRepository.java
+++ src/main/java/com/osh/backend/member/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package com.osh.backend.member.repository;
 import com.osh.backend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByMemberEmail(String memberEmail);
+    
 }
