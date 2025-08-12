2edf298c4e163a0e175f311a8deb2ce5c50aa2ec
오성현 <fhqht303@naver.com>
2025-08-13 03:30:33 +0900

    feat : filterChain 메소드 추가

--- src/main/java/com/osh/backend/config/SecurityConfig.java
+++ src/main/java/com/osh/backend/config/SecurityConfig.java
@@ -2,11 +2,27 @@ package com.osh.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests
+                            .requestMatchers("/*", "/**")
+                            .permitAll()
+                            .anyRequest().authenticated();
+                        });
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
