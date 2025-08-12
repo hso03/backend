589350549dc075c73d6132789e2358ae02badc24
오성현 <fhqht303@naver.com>
2025-08-12 22:33:10 +0900

    feat : CORS 도메인 추가

--- src/main/java/com/osh/backend/config/WebConfig.java
+++ src/main/java/com/osh/backend/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "http://api.tennismeet.live")
                         .allowedMethods("GET", "POST", "DELETE", "PUT")
                         .allowCredentials(true);
             }
