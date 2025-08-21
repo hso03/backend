8ffe9467d2a7e7857cc91819ffb8921c16daba9e
오성현 <fhqht303@naver.com>
2025-08-21 21:31:19 +0900

    feat : CustomMemberDetailsService 생성

---
+++ src/main/java/com/osh/backend/jwt/service/CustomMemberDetailsService.java
@@ -0,0 +1,22 @@
+package com.osh.backend.jwt.service;
+
+import com.osh.backend.advice.exception.MemberNotFoundException;
+import com.osh.backend.jwt.dto.CustomMemberDetails;
+import com.osh.backend.member.domain.Member;
+import com.osh.backend.member.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@AllArgsConstructor
+public class CustomMemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByMemberEmail(username).orElseThrow(() -> new MemberNotFoundException("멤버를 찾을 수 없습니다."));
+        return new CustomMemberDetails(member);
+    }
+}
