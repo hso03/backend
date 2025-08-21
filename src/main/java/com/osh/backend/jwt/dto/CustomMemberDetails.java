c7e3329ed1713b8fd00dfdc21b8774119fe4a9a7
오성현 <fhqht303@naver.com>
2025-08-21 21:31:07 +0900

    feat : CustomMemberDetails 생성

---
+++ src/main/java/com/osh/backend/jwt/dto/CustomMemberDetails.java
@@ -0,0 +1,58 @@
+package com.osh.backend.jwt.dto;
+
+import com.osh.backend.member.domain.Member;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class CustomMemberDetails implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public String getUsername() {
+        return member.getMemberEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getMemberPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getRole();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+}
