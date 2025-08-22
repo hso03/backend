deb3501f02ae5013345a197e31195cf61af7df03
오성현 <fhqht303@naver.com>
2025-08-22 18:32:01 +0900

    feat : TEST용 GetMapping 추가

--- src/main/java/com/osh/backend/controller/MemberController.java
+++ src/main/java/com/osh/backend/controller/MemberController.java
@@ -1,15 +1,13 @@
 package com.osh.backend.controller;
 
+import com.osh.backend.jwt.dto.LoginRequest;
 import com.osh.backend.member.dto.MemberRegisterRequest;
 import com.osh.backend.member.dto.MemberResponse;
 import com.osh.backend.member.service.impl.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/member")
@@ -22,6 +20,11 @@ public class MemberController {
     public ResponseEntity<MemberResponse> registerMember(@Validated @RequestBody MemberRegisterRequest memberRegisterRequest){
         MemberResponse memberResponse = memberService.registerMember(memberRegisterRequest);
         return ResponseEntity.ok(memberResponse);
+    }
+
+    @GetMapping("/zz")
+    public ResponseEntity<String> getMember(){
+        return ResponseEntity.ok("zz");
     }
 
 }
