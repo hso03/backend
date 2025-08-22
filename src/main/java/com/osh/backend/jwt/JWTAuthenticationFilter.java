cc9ec088a4c6caf66b284bc496319b3bea0712a0
오성현 <fhqht303@naver.com>
2025-08-22 18:30:23 +0900

    feat : jwt 인증하는 부춘 추가

--- src/main/java/com/osh/backend/jwt/JWTAuthenticationFilter.java
+++ src/main/java/com/osh/backend/jwt/JWTAuthenticationFilter.java
@@ -1,9 +1,16 @@
 package com.osh.backend.jwt;
 
+import com.osh.backend.jwt.dto.CustomMemberDetails;
+import com.osh.backend.jwt.service.CustomMemberDetailsService;
+import com.osh.backend.member.domain.Member;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -13,20 +20,40 @@ import java.util.Objects;
  * JWT 토큰의 유효기간이 남았는지 확인하는 필터
  * 즉, JWT 유효성 검증 담당
  */
+@RequiredArgsConstructor
 public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         // 요청 Header에서 JWT토큰인 "Authorization" 추출
-        String authorization = request.getHeader("Authorization");
-        if(Objects.isNull(authorization) || authorization.startsWith("Bearer ")){
-            filterChain.doFilter(request, response);
-            return;
+        String header = request.getHeader("Authorization");
+
+        if(Objects.nonNull(header) && header.startsWith("Bearer ")){
+            System.out.println("1");
+            // Bearer이후 token 추출
+            String token = header.split(" ")[1];
+            if(Objects.nonNull(token) && !jwtUtil.isExpired(token)){
+                System.out.println("2");
+                String username = jwtUtil.getUsername(token);
+
+                Member member = new Member(
+                        username,
+                        "temp",
+                        "temp",
+                        "temp"
+                );
+
+                CustomMemberDetails memberDetails = new CustomMemberDetails(member);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(memberDetails, null, memberDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            }
         }
 
-
-        String username = null;
-        String token = null;
+        filterChain.doFilter(request, response);
     }
 }
