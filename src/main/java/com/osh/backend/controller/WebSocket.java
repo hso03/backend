4dd86ca4cc4deff3f0a668ea8f596f6c0fdeb780
오성현 <fhqht303@naver.com>
2025-09-01 03:11:57 +0900

    feat : 채팅 컨트롤러

---
+++ src/main/java/com/osh/backend/controller/WebSocket.java
@@ -0,0 +1,19 @@
+package com.osh.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/web-socket")
+@RequiredArgsConstructor
+public class WebSocket {
+
+//    @PostMapping("/chat")
+//    public ResponseEntity<Void> sendMessage(ChatMessageRequest request) {
+//
+//    }
+
+}
